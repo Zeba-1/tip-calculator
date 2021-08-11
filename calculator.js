0f60c93822c1a771c4fbfcf45658d01094aaba7e
Zeba <75450120+Zeba-1@users.noreply.github.com>
2021-08-11 11:10:53 +0200

    CSS end and first JS

--- calculator.js
+++ calculator.js
@@ -1,3 +1,29 @@
 "use strict";
 
-let res
+var bill = document.getElementById('bill');
+var nPeople = document.getElementById("nPeople");
+
+var customTip = document.getElementById("customTip")
+
+var tipAmount = document.getElementById("tipAmount");
+var total = document.getElementById("total");
+
+bill.addEventListener('change', check);
+nPeople.addEventListener('change', check);
+customTip.addEventListener('change', tipSelector);
+tipAmount.addEventListener('change', check);
+total.addEventListener('change', check);
+
+function tipSelector(tip){
+    if (typeof(tip) == "object") {
+        selectTip = parseInt(customTip.value);
+    }else{
+        customTip.value = null;
+        selectTip = tip;
+    }
+    console.log(selectTip);
+}
+
+function check() {
+    console.log(selectTip);
+}
