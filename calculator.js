1f943e86d0657c3bf20c1bff485b1f91c0035337
Zeba <75450120+Zeba-1@users.noreply.github.com>
2021-08-15 17:04:50 +0200

    JS progression

--- calculator.js
+++ calculator.js
@@ -8,22 +8,103 @@ var customTip = document.getElementById("customTip")
 var tipAmount = document.getElementById("tipAmount");
 var total = document.getElementById("total");
 
+var b5 = document.getElementById("5");
+var b10 = document.getElementById("10");
+var b15 = document.getElementById("15");
+var b25 = document.getElementById("25");
+var b50 = document.getElementById("50");
+
 bill.addEventListener('change', check);
 nPeople.addEventListener('change', check);
 customTip.addEventListener('change', tipSelector);
-tipAmount.addEventListener('change', check);
-total.addEventListener('change', check);
+
 
 function tipSelector(tip){
+    resetButton();
     if (typeof(tip) == "object") {
-        selectTip = parseInt(customTip.value);
+        if (isNaN(parseInt(customTip.value)) == false)
+        {
+            selectTip = parseInt(customTip.value);
+        }else{
+            customTip.value = null;
+        }
     }else{
         customTip.value = null;
         selectTip = tip;
+
+        switch (tip) {
+            case 5: 
+                b5.classList.add("active");
+                break;
+            case 10: 
+                b10.classList.add("active");
+                break;
+            case 15: 
+                b15.classList.add("active");
+                break;
+            case 25: 
+                b25.classList.add("active");
+                break;
+            case 50: 
+                b50.classList.add("active");
+                break;
+        }
     }
-    console.log(selectTip);
+    check();
 }
 
+function resetButton() {
+    b5.classList.remove("active")
+    b10.classList.remove("active")
+    b15.classList.remove("active")
+    b25.classList.remove("active")
+    b50.classList.remove("active")
+}
+
+
 function check() {
-    console.log(selectTip);
+    if (bill.value != "") {
+        if (isNaN(parseInt(bill.value)))
+        {
+            bill.parentElement.classList.add("badInput")
+            bill.parentElement.classList.remove("activeInput")
+        }else{
+            bill.parentElement.classList.add("activeInput")
+            bill.parentElement.classList.remove("badInput")
+        }
+    }else
+    {
+        bill.parentElement.classList.remove("activeInput")
+        bill.parentElement.classList.remove("badInput")
+    }
+
+    if (nPeople.value != "") {
+        if (isNaN(parseInt(nPeople.value)))
+        {
+            nPeople.parentElement.classList.add("badInput")
+            nPeople.parentElement.classList.remove("activeInput")
+        }else{
+            nPeople.parentElement.classList.add("activeInput")
+            nPeople.parentElement.classList.remove("badInput")
+        }
+    }else
+    {
+        nPeople.parentElement.classList.remove("activeInput")
+        nPeople.parentElement.classList.remove("badInput")
+    }
+
+    if (customTip.value != "") {
+        if (isNaN(parseInt(customTip.value)))
+        {
+            customTip.parentElement.classList.add("badInput")
+            customTip.parentElement.classList.remove("activeInput")
+        }else{
+            customTip.parentElement.classList.add("activeInput")
+            customTip.parentElement.classList.remove("badInput")
+        }
+    }else
+    {
+        customTip.parentElement.classList.remove("activeInput")
+        customTip.parentElement.classList.remove("badInput")
+    }
 }
